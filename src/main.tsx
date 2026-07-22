0d57a8740facf11a7455791277974a43f35bc4fd
Rafael Soares <rafasoares96@gmail.com>
2026-07-21 21:14:52 -0300

    Adiciona tela de carregamento com logo Rafael.dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Esconde a tela de carregamento
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  if (loader) {
+    loader.classList.add("loader-hidden");
+
+    setTimeout(() => {
+      loader.remove();
+    }, 500);
+  }
+});
